8687341c1fdef425b6f71bb1360093cd22aa3040
Deepak Maurya <deepakrmaurya@outlook.com>
2026-02-13 03:04:45 +0530

    re-react Ep1

---
+++ App.js
@@ -0,0 +1,17 @@
+const heading = React.createElement(
+  "h1",
+  { id: "parent" },
+  React.createElement(
+    "h2",
+    { id: "child" },
+    React.createElement(
+      "h3",
+      {},
+      "This is heading coming from nested React Element",
+    ),
+  ),
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
