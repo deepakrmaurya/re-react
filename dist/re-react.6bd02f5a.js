a0b964381c085da2f621a9ec15e44b040ad83e14
Deepak Maurya <deepakrmaurya@outlook.com>
2026-04-08 22:29:12 +0530

    ep04

--- dist/re-react.6bd02f5a.js
+++
@@ -1,9 +0,0 @@
-const heading = React.createElement("h1", {
-    id: "parent"
-}, React.createElement("h2", {
-    id: "child"
-}, React.createElement("h3", {}, "This is heading coming from nested React Element")));
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
-//# sourceMappingURL=re-react.6bd02f5a.js.map
---
+++ src/App.js
@@ -0,0 +1,51 @@
+// **This is comment
+// *! This is also a comment
+// *? This is also a comment in blue
+// *TODO this is also a comment in orange
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./Components/Header";
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
+
+// const Title = () => {
+//   return <h1>This is the title component inside Header Component</h1>;
+// };
+
+// const Header = () => {
+//   return (
+//     <div>
+//       <Title />
+//       <h1 className="divTag">This is Functional Component</h1>
+//     </div>
+//   );
+// };
+
+// const jsxHeading = <h1 id="divTag">Hello, this is jsx</h1>;
+// console.log(jsxHeading);
+
+// const heading = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "h2",
+//     { id: "child" },
+//     React.createElement(
+//       "div",
+//       { id: "divTag" },
+//       "This is heading coming from nested React Element",
+//     ),
+//   ),
+// );
+// console.log(heading);
